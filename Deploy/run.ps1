5a4e9a3d585874f392bbd2810c3a944dca1a30bd
nastysilence <maxse828@gmail.com>
2021-07-27 00:02:25 +0300

    change deploy script to use existing composes

--- Deploy/run.ps1
+++ Deploy/run.ps1
@@ -1,2 +1,3 @@
-docker-compose down
-docker-compose up -d
+docker-compose -f ./Infrastructure/Mongo/docker-compose.mongo.yml up -d
+docker-compose -f ./Infrastructure/Redis/docker-compose.redis.yml up -d
+docker-compose -f ./StoreService/docker-compose.store.yml up -d
--- Deploy/run.sh
+++ Deploy/run.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
-docker-compose down
-docker-compose up -d
+docker-compose -f ./Infrastructure/Mongo/docker-compose.mongo.yml up -d
+docker-compose -f ./Infrastructure/Redis/docker-compose.redis.yml up -d
+docker-compose -f ./StoreService/docker-compose.store.yml up -d
