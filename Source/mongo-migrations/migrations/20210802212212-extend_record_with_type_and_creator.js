6f87f49513117ffa131b83b8baf4d3f405730f9f
nastysilence <maxse828@gmail.com>
2021-08-03 00:36:49 +0300

    add migrations

---
+++ Source/mongo-migrations/migrations/20210802212212-extend_record_with_type_and_creator.js
@@ -0,0 +1,15 @@
+module.exports = {
+  async up(db, client) {
+    // TODO write your migration here.
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    await db.collection('Records').updateMany({}, {$set: {RecordType: 0}});
+    await db.collection('Records').updateMany({}, {$set: {CreatedBy: "00000000-0000-0000-0000-000000000000"}});
+  },
+
+  async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  }
+};
