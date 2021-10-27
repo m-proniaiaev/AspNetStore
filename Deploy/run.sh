b8c68a9121e09cf6bb99b0bac137f9425b02c21d
nastysilence <maxse828@gmail.com>
2021-10-27 14:06:29 +0300

    edit deploy script

--- Deploy/run.sh
+++ Deploy/run.sh
@@ -2,3 +2,5 @@ docker-compose -f ./Infrastructure/Mongo/docker-compose.mongo.yml up -d
 docker-compose -f ./Infrastructure/Redis/docker-compose.redis.yml up -d
 docker-compose -f ./StoreService/docker-compose.store.internal.yml up -d
 docker-compose -f ./StoreService/docker-compose.store.auth.yml up -d
+cd ../Source/mongo-migrations || (echo "failed executing migrations" && exit)
+migrate-mongo up
